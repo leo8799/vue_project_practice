7bb17695b99aed3b73b4d090e4f51655096aac3e
leo <leo8799.tw@gmail.com>
2025-01-06 16:16:40 +0800

    feat: add lifePeriod.vue component
    學會了如何使用生命周期鈎子函數

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="app">
-    <!-- 加:則後面的字符串内容會轉爲標識符 -->
-    <!-- <h2 a="1+1" :b="1+1" c="x" :d="x">該標簽有四個props（屬性）</h2> -->
-    <!-- vue組件標簽的props屬性會傳遞給子組件，子組件可以編寫對應代碼覺得要不要接受，以及限制傳遞的數據類型 -->
-    <props a="哈哈" :list="personList"></props>
+    <!-- v-if 跟 v-show的區別在於，v-show不會卸載組件，只是將組件設爲不可視 -->
+    <lifePeriod v-if="isShow"></lifePeriod>
   </div>
 </template>
 
 <script lang="ts" setup name="App">
-  import { reactive } from 'vue';
-  import { type Persons } from './types';
-  import props from './components/props.vue';
+  import lifePeriod from './components/lifePeriod.vue';
+  import { onMounted, ref } from 'vue';
 
-  let x = 9
+  let isShow = ref(true)
 
-  // 標識符+<???>代表泛型，限制了數據類型
-  let personList = reactive<Persons>([
-  {id:'001', name: 'liuyaokai', age: 18},
-  {id:'002', name: 'liuyaokai', age: 18},
-  {id:'003', name: 'liuyaokai', age: 18}
-  ])
+
+  onMounted(()=>{
+    console.log('父組件挂載后')
+  })
 </script>
 
 <style>
---
+++ src/components/lifePeriod.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="life">
+    <h2>sum={{ sum }}</h2>
+    <button @click="add">sum+1</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="lifePeriod">
+  import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
+
+  let sum = ref(0)
+
+  function add(){
+    sum.value += 1
+  }
+
+  console.log('組件創建')
+
+  onBeforeMount(()=>{
+    console.log('組件挂載前')
+  })
+  onMounted(()=>{
+    console.log('組件挂載后')
+  })
+  onBeforeUpdate(()=>{
+    console.log('組件更新前')
+  })
+  onUpdated(()=>{
+    console.log('組件更新后')
+  })
+  onBeforeUnmount(()=>{
+    console.log('組件卸載前')
+  })
+  onUnmounted(()=>{
+    console.log('組件卸載後')
+  })
+</script>
+
+<style>
+  .life {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    paDDING: 20px;
+  }
+</style>
